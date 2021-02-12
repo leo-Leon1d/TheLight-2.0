6332b11391c41b2b9300e8e30201d9e3d5d6c5c8
Israelyan Leonid <leo.israelyan@icloud.com>
2021-02-12 16:29:03 +0300

    Added comments, hid the status bar and fixed the launch screen.

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -9,15 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+//Hide status bar
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+//Switch variable
     var isLightOn: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .black
     }
     
-    
-    
+//Function color update on the screen
     fileprivate func updateUI() {
         
         view.backgroundColor = isLightOn ? .white : .black
